dca0ec81323b45f5f54b339ff5289e26d706b688
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2021-03-26 19:40:49 +0200

    Update BankAcc.java

--- BankAcc.java
+++ BankAcc.java
@@ -1,52 +1,60 @@
 package bank_acc;
-public class BankAcc extends Account  implements Banking{
-    double Balance;
+
+public class BankAcc extends Account implements Banking {
+
+    private double Balance;
+
     public BankAcc(double Balance, Person owner, String Password) {
         super(owner, Password);
-        this.Balance = Balance;
-        add_Acc_To_Owner(owner);
+        this.setBalance(Balance);
+        this.add_Acc_To_Owner(owner);
     }
-    
-     public  void add_Acc_To_Owner(Person P1){
-        P1.accounts.add(this);
-      
-        
-      }
-     
-    
-   public void Deposit(double Amount){
-if(Amount>0){
-Balance +=Amount;
-}else{
-    System.out.println(" The Operation Is Canceled");
-    System.out.println(" The Amount Should Be Positive");
-    }
-}
-public void Withdraw(double Amount){
-if(Amount>0 && Amount<=Balance){
-Balance -=Amount;
-}else{
-  System.out.println(" The Operation Is Canceled");
-  System.out.println(" The Amount Should Be less than Amount");
-}
-} 
 
-    public void transfer(double Amount,BankAcc another){
-    if(Amount<=0){
-        System.out.println("Sorry, there is no balance in the account");
+    public void add_Acc_To_Owner(Person P1) {
+        P1.getAccounts().add(this);
+
     }
-    else if(Amount>Balance){
-        System.out.println("We regret that the process cannot be completed because the transfer value is greater than the balance in the account");
-    } 
-    else{
-    this.Balance -=Amount;
-    another.Balance +=Amount;
+
+    public double getBalance() {
+        return Balance;
     }
+
+    public void setBalance(double Balance) {
+        this.Balance = Balance;
+    }
+
+    public void Deposit(double Amount) {
+        if (Amount > 0) {
+            Balance += Amount;
+        } else {
+            System.out.println(" The Operation Is Canceled");
+            System.out.println(" The Amount Should Be Positive");
+        }
+    }
+
+    public void Withdraw(double Amount) {
+        if (Amount > 0 && Amount <= Balance) {
+            Balance -= Amount;
+        } else {
+            System.out.println(" The Operation Is Canceled");
+            System.out.println(" The Amount Should Be less than Amount");
+        }
+    }
+
+    public void transfer(double Amount, BankAcc another) {
+        if (Amount <= 0) {
+            System.out.println("Sorry, there is no balance in the account");
+        } else if (Amount > Balance) {
+            System.out.println("We regret that the process cannot be completed because the transfer value is greater than the balance in the account");
+        } else {
+            this.Balance -= Amount;
+            another.Balance += Amount;
+        }
     }
 
     @Override
     public String toString() {
-        return super.toString()+" , The Current Balance Is "+ Balance +"]";
+        return super.toString() + " , The Current Balance Is " + Balance + "]";
     }
-    }
+}
  
